17aec8fa1603c088871c9766e5a76ee8b81bc52d
souvik-maity-2002 <iamsouvikmaity@2002gmail.com>
2026-02-08 18:41:35 +0530

    refactor: update package name and author, enhance dependencies, and clean up user controller code

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,28 +1,28 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import {ApiError} from "../utils/ApiError.js"
-import { User} from "../models/user.model.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
-import { ApiResponse } from "../utils/ApiResponse.js";
-import jwt from "jsonwebtoken"
-import mongoose from "mongoose";
+// import {ApiError} from "../utils/ApiError.js"
+// import { User} from "../models/user.model.js"
+// import {uploadOnCloudinary} from "../utils/cloudinary.js"
+// import { ApiResponse } from "../utils/ApiResponse.js";
+// import jwt from "jsonwebtoken"
+// import mongoose from "mongoose";
 
 
-const generateAccessAndRefereshTokens = async(userId) =>{
-    try {
-        const user = await User.findById(userId)
-        const accessToken = user.generateAccessToken()
-        const refreshToken = user.generateRefreshToken()
+// const generateAccessAndRefereshTokens = async(userId) =>{
+//     try {
+//         const user = await User.findById(userId)
+//         const accessToken = user.generateAccessToken()
+//         const refreshToken = user.generateRefreshToken()
 
-        user.refreshToken = refreshToken
-        await user.save({ validateBeforeSave: false })
+//         user.refreshToken = refreshToken
+//         await user.save({ validateBeforeSave: false })
 
-        return {accessToken, refreshToken}
+//         return {accessToken, refreshToken}
 
 
-    } catch (error) {
-        throw new ApiError(500, "Something went wrong while generating referesh and access token")
-    }
-}
+//     } catch (error) {
+//         throw new ApiError(500, "Something went wrong while generating referesh and access token")
+//     }
+// }
 
 const registerUser = asyncHandler( async (req, res) => {
     // get user details from frontend
