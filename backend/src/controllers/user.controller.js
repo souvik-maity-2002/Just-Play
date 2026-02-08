463520b12479172ad60eb98d99e92125fc05be40
souvik-maity-2002 <iamsouvikmaity@2002gmail.com>
2026-02-08 18:41:59 +0530

    fix: restore token generation logic in user controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -7,22 +7,22 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 // import mongoose from "mongoose";
 
 
-// const generateAccessAndRefereshTokens = async(userId) =>{
-//     try {
-//         const user = await User.findById(userId)
-//         const accessToken = user.generateAccessToken()
-//         const refreshToken = user.generateRefreshToken()
+const generateAccessAndRefereshTokens = async(userId) =>{
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
 
-//         user.refreshToken = refreshToken
-//         await user.save({ validateBeforeSave: false })
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
 
-//         return {accessToken, refreshToken}
+        return {accessToken, refreshToken}
 
 
-//     } catch (error) {
-//         throw new ApiError(500, "Something went wrong while generating referesh and access token")
-//     }
-// }
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating referesh and access token")
+    }
+}
 
 const registerUser = asyncHandler( async (req, res) => {
     // get user details from frontend
