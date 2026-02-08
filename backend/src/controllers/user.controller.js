836235ed26e0847091afd2619fba9bc632440d7e
souvik-maity-2002 <iamsouvikmaity@2002gmail.com>
2026-02-08 18:45:42 +0530

    refactor: uncomment and implement logoutUser functionality in user controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -158,30 +158,30 @@ const loginUser = asyncHandler(async (req, res) =>{
 
 })
 
-// const logoutUser = asyncHandler(async(req, res) => {
-//     await User.findByIdAndUpdate(
-//         req.user._id,
-//         {
-//             $unset: {
-//                 refreshToken: 1 // this removes the field from document
-//             }
-//         },
-//         {
-//             new: true
-//         }
-//     )
+const logoutUser = asyncHandler(async(req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset: {
+                refreshToken: 1 // this removes the field from document
+            }
+        },
+        {
+            new: true
+        }
+    )
 
-//     const options = {
-//         httpOnly: true,
-//         secure: true
-//     }
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
 
-//     return res
-//     .status(200)
-//     .clearCookie("accessToken", options)
-//     .clearCookie("refreshToken", options)
-//     .json(new ApiResponse(200, {}, "User logged Out"))
-// })
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out"))
+})
 
 // const refreshAccessToken = asyncHandler(async (req, res) => {
 //     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
